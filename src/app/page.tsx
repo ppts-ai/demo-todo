02b41f7c49040f5a98e19e08e9260ce78fda6b02
p2pmax <jasonhao518@gmail.com>
2024-09-22 15:22:14 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,9 +32,7 @@ export default function Home() {
        **/}
 
       <CopilotKit
-        publicApiKey={process.env.NEXT_PUBLIC_COPILOT_CLOUD_API_KEY}
-        // Alternatively, you can use runtimeUrl to host your own CopilotKit Runtime
-        // runtimeUrl="/api/copilotkit"
+        runtimeUrl="/api/copilotkit"
       >
         <TodoList />
 
